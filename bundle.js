b383724ddc08657a0333628cfc3826d2d5352a62
jamesevers <jevers@gm.slc.edu>
2017-03-06 22:30:33 -0500

    Add new styles

--- bundle.js
+++ bundle.js
@@ -200,7 +200,7 @@ var Maps = {
 
     d3.json("https://d3js.org/us-10m.v1.json", function (error, us) {
       if (error) throw error;
-      statesGroup.selectAll("path").attr("class", "states").data(topojson.feature(us, us.objects.states).features).enter().append("path").attr("d", path).attr("fill", "#1D2951").attr("opacity", .8);
+      statesGroup.selectAll("path").attr("class", "states").data(topojson.feature(us, us.objects.states).features).enter().append("path").attr("d", path).attr("fill", "#3850a0").attr("opacity", .8);
 
       svg.append("path").attr("class", "border").attr("d", path(topojson.mesh(us, us.objects.states, function (a, b) {
         return a !== b;
@@ -219,7 +219,7 @@ var Maps = {
     d3.json("https://d3js.org/us-10m.v1.json", function (error, us) {
       if (error) throw error;
 
-      svg.append("g").attr("class", "states").selectAll("path").data(topojson.feature(us, us.objects.states).features).enter().append("path").attr("d", path).attr("fill", "#1D2951").attr("opacity", .8);
+      svg.append("g").attr("class", "states").selectAll("path").data(topojson.feature(us, us.objects.states).features).enter().append("path").attr("d", path).attr("fill", "blue").attr("opacity", .8);
 
       svg.append("path").attr("class", "border").attr("d", path(topojson.mesh(us, us.objects.states, function (a, b) {
         return a !== b;
@@ -243,7 +243,7 @@ var Maps = {
       return projection(coord)[1] + 55; // + 55
     }).attr("r", "1px").attr("fill", "url(#RadialGradient)").attr("class", "tweet-initial").style("opacity", .8).on("mouseover", function (d) {
       div.transition().duration(200).style("opacity", .9).style("fill", "yellow");
-      div.html(d.text).style("left", d3.event.pageX + "px").style("top", d3.event.pageY - 28 + "px").style("color", "white");
+      div.html(d.text).style("left", d3.event.pageX + "px").style("top", d3.event.pageY - 28 + "px").style("background", "white").style("color", "#1D2951");
     }).on("mouseout", function (d) {
       div.transition().duration(500).style("opacity", 0);
     });
@@ -18786,6 +18786,12 @@ module.exports = {
     Util.topWordCounts(wordCounts);
     Util.getDisplay(hourlyTweets);
     Maps.placeCoords(hourlyTweets);
+  },
+
+  searchTweets: function searchTweets(newValue) {
+    Maps.clearCoords();
+    var allData = Util.loadAllData();
+    var searchedTweets = Util.searchByTerm(allData, newValue);
   }
 
 };
